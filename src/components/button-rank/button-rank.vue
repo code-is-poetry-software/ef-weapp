e4d221c49c38822f1a6d2e7554b43b2f896380e5
uuz <ncysatnaf@gmail.com>
2020-07-02 17:37:23 +0800

    update rank page

---
+++ src/components/button-rank/button-rank.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="button-rank" @click="$emit('click')">
+    <img class="img" src="/static/image/button-rank.png" mode="widthFix" />
+    <view class="text">{{ text }}</view>
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Prop } from "vue-property-decorator";
+
+@Component
+export default class Template extends Vue {
+  @Prop({ default: "" }) text: string;
+}
+</script>
+
+<style lang="stylus" scoped>
+.button-rank
+  position relative
+  width 445upx
+  height 72upx
+  display flex
+  align-items center
+  justify-content center
+  .img
+    position absolute
+    width 100%
+    right 0
+    top 0
+  .text
+    position absolute
+    left 70upx
+    top 12upx
+    font-size 41upx
+    font-family Alibaba PuHuiTi
+    font-weight bold
+    letter-spacing 2px
+    color white
+    z-index 1
+    font-style italic
+</style>
--- src/pages/rank/index.vue
+++ src/pages/rank/index.vue
@@ -1,10 +1,21 @@
 <template>
   <view class="rank">
-    <view>EF PARK排行榜</view>
-    <view @click="navigateTo({ url: '/pages/rank/detail' })">
-      <view>竞速无人机排行榜</view>
-      <view>对战无人机排行榜</view>
-      <view>动力方程赛车排行榜</view>
+    <view style="margin-left: 60upx;margin-top:60upx">
+      <button-rank text="EF PARK排行榜" />
+    </view>
+    <view>
+      <img class="icon-user" src="/static/image/icon-user.png" mode="widthFix" />
+    </view>
+    <view class="column-center" style="margin-top:100upx" @click="navigateTo({ url: '/pages/rank/detail' })">
+      <view style="margin-top: 50upx">
+        <button-rank-item text="竞速无人机排行榜" />
+      </view>
+      <view style="margin-top: 50upx">
+        <button-rank-item text="对战无人机排行榜" />
+      </view>
+      <view style="margin-top: 50upx">
+        <button-rank-item text="动力方程赛车排行榜" />
+      </view>
     </view>
   </view>
 </template>
@@ -21,6 +32,12 @@ export default class RankIndex extends Vue {
 </script>
 
 <style lang="stylus" scoped>
-.title
-  font-size 20upx
+.rank
+  padding 120upx 0
+  .icon-user
+    width 41px
+    height 36px
+    position absolute
+    right 61upx
+    top 65upx
 </style>
