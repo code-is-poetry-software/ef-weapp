23c1e361255fac9f026f0708102d78138343b8d0
uuz <ncysatnaf@gmail.com>
2020-07-12 18:56:40 +0800

    update style

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="index">
+  <view class="index page">
     <get-phonenumber />
     <login />
     <with-bg :showTop="false" />
--- src/pages/user/history.vue
+++ src/pages/user/history.vue
@@ -1,12 +1,21 @@
 <template>
   <view class="user-history page">
     <with-bg />
-    <view style="padding:140upx 62upx 113upx 58upx" class="flex justify-between align-center">
+    <view style="padding:140upx 62upx 233upx 58upx" class="flex justify-between align-center">
       <button-title1 text="潮玩记录" />
       <icon-details />
     </view>
-    <view>
-      <button-avatar5 :user="user" />
+    <view class="content">
+      <view class="user-info">
+        <button-avatar5 :user="user" />
+      </view>
+      <border1>
+        <view class="list">
+          <view class="list-item" v-for="(item, index) in curList" :key="item">
+            <button-avatar4 :rank="index + 1" />
+          </view>
+        </view>
+      </border1>
     </view>
   </view>
 </template>
@@ -17,6 +26,44 @@ import { authStore } from "../../store/auth";
 
 @Component
 export default class Template extends Vue {
+  curList = [
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    },
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    },
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    },
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    },
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    },
+    {
+      id: 0,
+      name: "竞速无人机",
+      date: "2020年6月8日",
+      score: "5:0"
+    }
+  ];
   onLoad() {
     authStore.devLogin();
   }
@@ -29,19 +76,24 @@ export default class Template extends Vue {
 <style lang="stylus" scoped>
 .user-history
   position relative
-  .tabs
-    display flex
-    justify-content center
-    .tab
+  text-align center
+  .content
+    margin 0 auto
+    width 629upx
+    position relative
+    .user-info
       display flex
-      align-items center
-      padding 40upx
-      width 200upx
-      height 56upx
-      background grey
-      margin 0 20upx
-      &.active
-        background #4d8ad6
-  .list
-    text-align center
+      position absolute
+      left 0
+      text-align center
+      z-index 10
+      top -160upx
+      justify-content center
+      width 100%
+    .list
+      text-align center
+      padding-top 140upx
+    .list-item
+      position relative
+      transform scale(0.8)
 </style>
